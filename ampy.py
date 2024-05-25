c001a377119b1a4fbd2133e2c0db9c25f2baf1f2
nati <nwtbekele@gmail.com>
2024-05-25 06:06:51 -0500

    Logos

---
+++ ampy.py
@@ -0,0 +1,22 @@
+
+import math
+import numpy as np
+
+for i in range(1, 100, 2):
+    print(i*20) 
+    # multiplication
+    print(i**20) # exponent
+
+c = 0
+while (c < 3):
+    print(math.sin(c)) 
+    ''' sin of a value 
+
+
+    '''
+    c+=1
+
+if c == 3:
+    print("This is working")
+else:
+    print("This is also working")
