69274e393b0d6f7a2592b57dd9c989e461f49954
nati <nwtbekele@gmail.com>
2024-09-14 22:20:59 -0500

    linting

--- src/generate_bins.cpp
+++ src/generate_bins.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
 #include <fstream>
-#include <filesystem>
+#include <iostream>
 #include <json/json.h>
 
 void generate_builtin_key_bin() {
-  std::ofstream bin_out ("./../data/builtin_keywords.bin", std::ios_base::binary);
+  std::ofstream bin_out("./../data/builtin_keywords.bin",
+                        std::ios_base::binary);
 
-  std::ifstream json_file ("./../data/builtin_keywords.json", std::ifstream::binary);
+  std::ifstream json_file("./../data/builtin_keywords.json",
+                          std::ifstream::binary);
   Json::Value myJson;
   json_file >> myJson;
 
@@ -16,9 +17,11 @@ void generate_builtin_key_bin() {
   std::string map_size = std::to_string(myJson.size());
   bin_out.write(map_size.c_str(), map_size.length() + 1);
 
-  for (Json::Value::const_iterator it = myJson.begin(); it != myJson.end(); ++it) {
-    bin_out.write(it.key().asString().c_str(),it.key().asString().length() + 1);
-    bin_out.write((*it).asString().c_str(),(*it).asString().length() + 1);
+  for (Json::Value::const_iterator it = myJson.begin(); it != myJson.end();
+       ++it) {
+    bin_out.write(it.key().asString().c_str(),
+                  it.key().asString().length() + 1);
+    bin_out.write((*it).asString().c_str(), (*it).asString().length() + 1);
   }
 
   bin_out.close();
@@ -26,16 +29,19 @@ void generate_builtin_key_bin() {
 }
 
 void generate_library_key_bin() {
-  std::ofstream bin_out ("./../data/library_keywords.bin", std::ios_base::binary);
+  std::ofstream bin_out("./../data/library_keywords.bin",
+                        std::ios_base::binary);
 
-  std::ifstream json_file ("./../data/library_keywords.json", std::ifstream::binary);
+  std::ifstream json_file("./../data/library_keywords.json",
+                          std::ifstream::binary);
   Json::Value myJson;
   json_file >> myJson;
 
   // std::cout << myJson << '\n';
 
   int total_size = 0;
-  for (Json::Value::const_iterator it = myJson.begin(); it != myJson.end(); ++it) 
+  for (Json::Value::const_iterator it = myJson.begin(); it != myJson.end();
+       ++it)
     total_size += (*it).size();
 
   std::cout << total_size << " : LIBRARY KEYWORDS JSON SIZE\n";
@@ -43,12 +49,17 @@ void generate_library_key_bin() {
   std::string map_size = std::to_string(total_size);
   bin_out.write(map_size.c_str(), map_size.length() + 1);
 
-  for (Json::Value::const_iterator it = myJson.begin(); it != myJson.end(); ++it) {
-    bin_out.write(it.key().asString().c_str(), it.key().asString().length() + 1);
+  for (Json::Value::const_iterator it = myJson.begin(); it != myJson.end();
+       ++it) {
+    bin_out.write(it.key().asString().c_str(),
+                  it.key().asString().length() + 1);
     auto inner_map = (*it);
-    for (Json::Value::const_iterator inner_it = inner_map.begin(); inner_it != inner_map.end(); ++inner_it) {
-      bin_out.write(inner_it.key().asString().c_str(), inner_it.key().asString().length() + 1);
-      bin_out.write((*inner_it).asString().c_str(), (*inner_it).asString().length() + 1);
+    for (Json::Value::const_iterator inner_it = inner_map.begin();
+         inner_it != inner_map.end(); ++inner_it) {
+      bin_out.write(inner_it.key().asString().c_str(),
+                    inner_it.key().asString().length() + 1);
+      bin_out.write((*inner_it).asString().c_str(),
+                    (*inner_it).asString().length() + 1);
     }
     bin_out.write("-1", 3);
   }
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,42 +1,42 @@
-#include <iostream>
-#include <filesystem>
-#include <unistd.h>
-#include <fstream>
-#include <cassert>
 #include "ampy.hpp"
+#include <cassert>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
 
-bool is_ampy(std::string file_name)
-{
-  /* Verifies if the file exists and has an .ampy extension. Exits with code 1 if neither is true.
-   * 
-   * Args: 
+bool is_ampy(std::string file_name) {
+  /* Verifies if the file exists and has an .ampy extension. Exits with code 1
+   * if neither is true.
+   *
+   * Args:
    *    file_name: The filename.
-   * 
+   *
    * Returns true if the function did not exit.
    */
-    
-  if (!std::filesystem::exists(file_name)){
+
+  if (!std::filesystem::exists(file_name)) {
     std::cout << file_name << " file does not exist.\n";
     exit(1);
   }
 
   std::filesystem::path fpath = file_name;
-  if (fpath.extension() != ".ampy"){
+  if (fpath.extension() != ".ampy") {
     std::cout << file_name << " is not a .ampy file.\n";
     exit(1);
   }
 
-  std::ifstream original_file (file_name);
-  if (!original_file.is_open()){
+  std::ifstream original_file(file_name);
+  if (!original_file.is_open()) {
     std::cout << "Error with file " << file_name << std::endl;
     exit(1);
   }
 
-  std::filesystem::path path {file_name}, ext (".py");
-  /* 
+  std::filesystem::path path{file_name}, ext(".py");
+  /*
    * DO THIS IN The function that called is_ampy
   std::ofstream output_file (path.replace_extension(ext));
-    
+
   if (!output_file.is_open()){
     std::cout << "Error with creating the output file " << path << std::endl;
     exit(1);
@@ -45,15 +45,17 @@ bool is_ampy(std::string file_name)
   return true;
 }
 
-library_keywords read_library_keywords_from_bin(std::ifstream &library_bin_input) {
-  /* Reads the `library_keywords.bin` binary and uses it to fill the `library_keywords` 
-   * `unordered_map`. Compares the number of the keywords read and the count stated in the file 
-   * to check if the binary has been manipulated. Also, makes sure that the last pair reading stops
-   * when a key can't be read.
+library_keywords
+read_library_keywords_from_bin(std::ifstream &library_bin_input) {
+  /* Reads the `library_keywords.bin` binary and uses it to fill the
+   * `library_keywords` `unordered_map`. Compares the number of the keywords
+   * read and the count stated in the file to check if the binary has been
+   * manipulated. Also, makes sure that the last pair reading stops when a key
+   * can't be read.
    *
    * Args:
-   *    library_bin_input: The input filestream for the binary containing the `Ampy` keywords for 
-   *                      python library functions and keywords.
+   *    library_bin_input: The input filestream for the binary containing the
+   * `Ampy` keywords for python library functions and keywords.
    *
    * Returns the generated `library_keywords`.
    */
@@ -71,7 +73,8 @@ library_keywords read_library_keywords_from_bin(std::ifstream &library_bin_input
     exit(1);
   }
 
-  // -1 should be used to mark end of library. It would therefore, show the start as well
+  // -1 should be used to mark end of library. It would therefore, show the
+  // start as well
   if (!std::getline(library_bin_input, lib, '\0')) {
     std::cout << "`library_keywords.bin` binary is corrupt.\n";
     exit(1);
@@ -81,38 +84,41 @@ library_keywords read_library_keywords_from_bin(std::ifstream &library_bin_input
     if (key == "-1") {
       // a new library's keywords have been reached
       library_map[lib] = inner_map;
-      if (!std::getline(library_bin_input, lib, '\0')) break;
+      if (!std::getline(library_bin_input, lib, '\0'))
+        break;
       str_map inner_map;
-    } 
-    else if (!std::getline(library_bin_input, val, '\0')) {
+    } else if (!std::getline(library_bin_input, val, '\0')) {
       std::cout << "`library_keywords.bin` binary is corrupt.\n";
       exit(1);
-    } 
-    else {
+    } else {
       inner_map[key] = val;
-      read_keyword_count ++;
+      read_keyword_count++;
     }
   }
-  
+
   if (stoi(keywords_size) != read_keyword_count) {
-    std::cout << "\nERROR => Read " << read_keyword_count << " | Expected " << stoi(keywords_size) << '\n';
+    std::cout << "\nERROR => Read " << read_keyword_count << " | Expected "
+              << stoi(keywords_size) << '\n';
     std::cout << "Download the correct library keyword binary.";
     exit(1);
   }
-  std::cout << read_keyword_count << " pairs successfully extracted from `library_keywords.bin`.\n";
+  std::cout << read_keyword_count
+            << " pairs successfully extracted from `library_keywords.bin`.\n";
 
   return library_map;
 }
 
-builtin_keywords read_builtin_keywords_from_bin(std::ifstream &builtin_bin_input) {
-  /* Reads the `builtin_keywords.bin` binary and uses it to fill the `builtin_keywords` 
-   * `unordered_map`. Compares the number of the keywords read and the count stated in the file 
-   * to check if the binary has been manipulated. Also, makes sure that the last pair reading stops
-   * when a key can't be read.
+builtin_keywords
+read_builtin_keywords_from_bin(std::ifstream &builtin_bin_input) {
+  /* Reads the `builtin_keywords.bin` binary and uses it to fill the
+   * `builtin_keywords` `unordered_map`. Compares the number of the keywords
+   * read and the count stated in the file to check if the binary has been
+   * manipulated. Also, makes sure that the last pair reading stops when a key
+   * can't be read.
    *
    * Args:
-   *    builtin_bin_input: The input filestream for the binary containing the `Ampy` keywords for 
-   *                      python builtin functions and keywords.
+   *    builtin_bin_input: The input filestream for the binary containing the
+   * `Ampy` keywords for python builtin functions and keywords.
    *
    * Returns the generated `builtin_keywords`.
    */
@@ -134,48 +140,54 @@ builtin_keywords read_builtin_keywords_from_bin(std::ifstream &builtin_bin_input
       std::cout << "`builtin_keywords.bin` binary is corrupt.\n";
       exit(1);
     }
-    builtin_map[key] = val;    
+    builtin_map[key] = val;
     read_keyword_count++;
   }
 
   if (stoi(key_words_size) != read_keyword_count) {
-    std::cout << "\nERROR => Read " << read_keyword_count << " | Expected " << stoi(key_words_size) << '\n';
+    std::cout << "\nERROR => Read " << read_keyword_count << " | Expected "
+              << stoi(key_words_size) << '\n';
     std::cout << "Download the correct builtin keyword binary.";
     exit(1);
   }
 
-  std::cout << read_keyword_count << " pairs successfully extracted from `builtin_keywords.bin`.\n";
+  std::cout << read_keyword_count
+            << " pairs successfully extracted from `builtin_keywords.bin`.\n";
   return builtin_map;
 }
 
-void read_keys_from_bin(builtin_keywords &builtin_map, library_keywords &library_map) {
-  /* Function to help read both `builtin_keywords.bin` and `library_keywords.bin` using their
-   * respective functions. Makes sure the files can be accessed and creates input filestreams for 
-   * them to be used by the functions that generate the `unordered_map` for either.
+void read_keys_from_bin(builtin_keywords &builtin_map,
+                        library_keywords &library_map) {
+  /* Function to help read both `builtin_keywords.bin` and
+   * `library_keywords.bin` using their respective functions. Makes sure the
+   * files can be accessed and creates input filestreams for them to be used by
+   * the functions that generate the `unordered_map` for either.
    *
    * Args:
-   *    builtin_map: Reference of a `builtin_keywords` `unordered_map` that will be filled with 
-   *        what is read from the `builtin_keywords.bin` binary.
-   *    library_map: Reference of a `library_keywords` `unordered_map` that will be filled with 
-   *        what is read from the `library_keywords.bin` binary.
+   *    builtin_map: Reference of a `builtin_keywords` `unordered_map` that will
+   * be filled with what is read from the `builtin_keywords.bin` binary.
+   *    library_map: Reference of a `library_keywords` `unordered_map` that will
+   * be filled with what is read from the `library_keywords.bin` binary.
    */
-  if (access( "./../data/builtin_keywords.bin", F_OK ) == -1 ){
+  if (access("./../data/builtin_keywords.bin", F_OK) == -1) {
     std::cout << "Error accessing with the `builtin_keywords` binary\n";
     exit(1);
   }
 
-  if (access( "./../data/library_keywords.bin", F_OK ) == -1 ){
+  if (access("./../data/library_keywords.bin", F_OK) == -1) {
     std::cout << "Error accessing with the `library_keywords` binary\n";
     exit(1);
   }
 
-  std::ifstream builtin_bin_input ("./../data/builtin_keywords.bin", std::ios_base::binary);
+  std::ifstream builtin_bin_input("./../data/builtin_keywords.bin",
+                                  std::ios_base::binary);
   if (!builtin_bin_input.is_open()) {
     std::cout << "Error opening with the `builtin_keywords` binary\n";
     exit(1);
   }
 
-  std::ifstream library_bin_input ("./../data/library_keywords.bin", std::ios_base::binary);
+  std::ifstream library_bin_input("./../data/library_keywords.bin",
+                                  std::ios_base::binary);
   if (!library_bin_input.is_open()) {
     std::cout << "Error opening with the `library_keywords` binary\n";
     exit(1);
@@ -187,12 +199,14 @@ void read_keys_from_bin(builtin_keywords &builtin_map, library_keywords &library
   library_bin_input.close();
 }
 
-int main(){
+int main() {
   builtin_keywords builtin_map;
   library_keywords library_map;
   read_keys_from_bin(builtin_map, library_map);
   assert(builtin_map["ከሆነ"] == "if");
   assert(library_map["ባየሽ"]["ባየሽ"] == "random");
   assert(library_map["ጊዜ"]["ጊዜ"] == "time");
-  std::cout << "ከሆነ = " << builtin_map["ከሆነ"] << " ባየሽ = " << library_map["ባየሽ"]["ባየሽ"] << " ጊዜ = " << library_map["ጊዜ"]["ጊዜ"];
+  std::cout << "ከሆነ = " << builtin_map["ከሆነ"]
+            << " ባየሽ = " << library_map["ባየሽ"]["ባየሽ"]
+            << " ጊዜ = " << library_map["ጊዜ"]["ጊዜ"];
 }
